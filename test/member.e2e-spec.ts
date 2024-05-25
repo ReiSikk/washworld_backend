0c30d0347abf5d508736e9e454337ba1b688b82b
Michalina <michalina.oniszczuk@gmail.com>
2024-05-25 16:53:51 +0200

    added member e2e

---
+++ test/member.e2e-spec.ts
@@ -0,0 +1,78 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { CreateMemberDto } from 'src/member/dto/create-member.dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm'
+import { Member } from '../src/member/entities/member.entity';
+import { MemberService } from '../src/member/member.service';
+import { AuthService } from '../src/auth/auth.service';
+
+describe('UserController (e2e)', () => {
+    let app: INestApplication;
+    let memberService: MemberService;
+    let authService: AuthService;
+    let memberRepository: Repository<Member>;
+    //let connection: DataSource;
+  
+    beforeEach(async () => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+
+      memberService = moduleFixture.get(MemberService);
+      authService = moduleFixture.get(AuthService);
+      memberRepository = moduleFixture.get(getRepositoryToken(Member))
+      
+      //connection = moduleFixture.get(DataSource)
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    });
+
+
+       describe('Sign up flow', () => {
+        it('should create a new member', async () => {
+    
+            const validMember = {
+                email: 'user@user.com',
+                password: 'testpassword',
+                firstName: 'Name',
+                lastName: 'Lastname',
+                phone: '12345',
+            };
+    
+            const {body} = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send(validMember)
+            .expect(201)
+    
+            console.log("saved member", body);
+            expect(body.email).toEqual(validMember.email)
+            expect(body.id).toBeDefined()
+        })
+
+
+       /*  it('should return error message when passed an invalid member', async () => {
+            const invalidMember: Partial<CreateMemberDto> = {
+                email: 'user@user.com',
+                password: 'bla',
+                firstName: 'name',
+                lastName: 'lastname',
+                phone: '12345',
+            };
+    
+          const {body} = await request(app.getHttpServer())
+          .post('/member')
+          .send(invalidMember)
+          .expect(400)
+    
+          expect(body.message[0]).toEqual('Password must be at least 8 characters long');
+        }) */
+        
+      }) 
+      afterAll(() => {
+        app.close();
+      });
+    });
