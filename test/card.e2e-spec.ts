4e2d0efddb26da7adda3866e0cc42ca5246ccdc1
Rei Sikk <sikkrei@gmail.com>
2024-05-26 19:57:10 +0200

    card e2e added

---
+++ test/card.e2e-spec.ts
@@ -0,0 +1,87 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { PaymentCard } from '../src/payment-card/entities/payment-card.entity';
+import { CreatePaymentCardDto } from '../src/payment-card/dto/create-payment-card.dto';
+
+describe('CardController (e2e)', () => {
+    let app: INestApplication;
+    let cardRepository: Repository<PaymentCard>;
+    let connection: DataSource;
+    let createdCardId: number;
+  
+    beforeEach(async () => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+
+      cardRepository = moduleFixture.get(getRepositoryToken(PaymentCard))
+      
+      connection = moduleFixture.get(DataSource)
+      app = moduleFixture.createNestApplication();
+      app.useGlobalPipes(new ValidationPipe());
+      await app.init();
+    });
+
+
+       describe('Add card to db', () => {
+        it('should create a new card entry in the db', async () => {
+
+          const createdCard : CreatePaymentCardDto = {
+            nameOnCard: 'George Brown',
+            cardNumber: '6782 1234 1234 1234',
+            expirationDate: new Date(),
+            cvv: '123',
+        };
+            
+          const { body } = await request(app.getHttpServer())
+                              .post('/paymentcard')
+                              .send(createdCard)
+                              .expect(201);
+
+                              expect(body.id).toBeDefined();
+                              expect(body.nameOnCard).toEqual(createdCard.nameOnCard);
+                              expect(body.cardNumber).toEqual(createdCard.cardNumber);
+                              expect(body.expirationDate).toEqual(createdCard.expirationDate.toISOString());
+                              expect(body.cvv).toEqual(createdCard.cvv);
+                              createdCardId = body.id;
+                            
+        })
+
+        afterEach(async () => {
+            if(createdCardId) {
+                await cardRepository.delete(createdCardId);
+            }
+        })
+
+      })
+       describe('Trying to add duplicate card', () => {
+        it('should not create a new card entry in the db', async () => {
+
+          const createdCard : CreatePaymentCardDto = {
+            nameOnCard: 'Markus Brown',
+            cardNumber: '1234 1234 1234 1234',
+            expirationDate: new Date(),
+            cvv: '123',
+        };
+            
+          const { body } = await request(app.getHttpServer())
+                              .post('/paymentcard')
+                              .send(createdCard)
+                              .expect(409);
+                              
+                              expect(body.message).toEqual('Card already added to the system');
+                            
+        })
+      })
+
+
+
+      afterAll(() => {
+        app.close();
+      });
+    });
